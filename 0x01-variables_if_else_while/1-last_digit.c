11e1e862e8b4f95396f82b78f12ec60fbb8cba0f
black99hard <yasser99hard@gmail.com>
2023-02-17 16:38:05 +0100

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* find the last digit of the given random numbers */
+/**
+* main - to find last number 
+* @n to to insert into an int a value of random numbers
+* @lastDigit will take last digit of n random number 
+* 
+*/
 int main(void)
 {
 	int n;
@@ -11,17 +16,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    lastDigit = n % 10;
-	
-	if(lastDigit > 5) 
-		printf("Last digit of %d is %d and is greater than 5\n",n,lastDigit);
-	if(lastDigit == 0) 
-		printf("Last digit of %d is %d and is 0\n",n,lastDigit);
-	if(lastDigit < 6 && lastDigit != 0 )
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastDigit);
-	
+	lastDigit = n % 10;
+
+	if (lastDigit > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	if (lastDigit == 0)
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	if (lastDigit < 6 && lastDigit != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	/* your code goes there */
-	
-	
 	return (0);
 }
