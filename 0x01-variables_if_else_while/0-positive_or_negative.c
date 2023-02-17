17b469edd36a1c010bc4b21631d7720a9dbb9216
black99hard <yasser99hard@gmail.com>
2023-02-17 16:52:11 +0100

    d

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/*here is the header of loader*/
 
 /**
-*this is a main function that tak all the code I may type 
+*main - to get random number out of the use of srandom function
+*@n this take a random value genarted from srandom functions
+*Return: success if went well
 */
-int main(void){
+int main(void)
+{
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n < 0)
+
+	if (n < 0)
 		printf("is negtaive");
-	if(n > 0)
+	if (n > 0)
 		printf("is postive");
-	if(n == 0)
+	if (n == 0)
 		printf("is zero");
-	return(0);
+	return (0);
 
 }
