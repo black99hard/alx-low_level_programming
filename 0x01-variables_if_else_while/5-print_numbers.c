bf95d74bea013bd52a7c56dcb1b2cb5150de16bc
black99hard <yasser99hard@gmail.com>
2023-02-18 10:23:10 +0100

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * *main - find the last digit of the given random numbers
+ * @ch to insert the characters fuond during the  looping
+ * Return: return success if went well
+ */
+int main(void)
+{
+int numbers;
+
+for (numbers = 0; numbers <= 9 ; numbers++)
+{
+printf("%d", numbers);
+
+}
+
+printf("\n");
+
+return (0);
+}
