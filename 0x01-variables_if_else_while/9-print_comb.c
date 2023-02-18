4597ea4706959fddbb96554c09bc67d1caf3aa54
black99hard <yasser99hard@gmail.com>
2023-02-18 11:33:31 +0100

    d

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * *main - find the last digit of the given random numbers
+ * @ch to insert the characters fuond during the  looping
+ * Return: return success if went well
+ */
+int main(void)
+{
+int ch;
+
+for (ch = '0'; ch <= '9' ; ch++)
+{
+if(ch == '9')
+{
+putchar(ch);
+putchar(32);
+break;
+}
+putchar(ch);
+putchar(44);
+putchar(32);
+
+}
+
+putchar(10);
+
+return (0);
+}
