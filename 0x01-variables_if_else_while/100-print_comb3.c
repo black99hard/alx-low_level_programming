1235e7bdc4528c1a19ff8ba00a87507700960e4a
black99hard <yasser99hard@gmail.com>
2023-02-18 14:35:04 +0100

    s

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
 /**
-* main - Entry point
-* Description - Combinations of two digit
-* Return: 0
-*/
-
+ * *main - find the last digit of the given random numbers
+ * @ch to insert the characters fuond during the  looping
+ * Return: return success if went well
+ */
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            putchar('0' + i);
-            putchar('0' + j);
-            if (i < 8)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+for (i = 0; i <= 8; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar(10);
+return (0);
+
+return (0);
 }
