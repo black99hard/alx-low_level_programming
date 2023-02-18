117f04b91bf39802879bce03011eb7f9f49cbcf3
black99hard <yasser99hard@gmail.com>
2023-02-18 15:43:40 +0100

    d

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    
+
+    printf("Size of a char: 1 byte( %d)\n", sizeof(char));
+    printf("Size of an int: 4 byte(%d)\n", sizeof(int));
+    printf("Size of a long int: 4 byte(%d)\n", sizeof(long int));
+    printf("Size of a long long int: 8 byte(%d)\n", sizeof(long long int));
+    printf("Size of a float: 4 byte(%d)\n", sizeof(float));
+
+
+    return (0);
+}
+
+
+
+
+
