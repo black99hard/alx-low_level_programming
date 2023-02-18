7f12a30ca95f24c24194d78d3033f1558528396d
black99hard <yasser99hard@gmail.com>
2023-02-18 23:43:38 +0100

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,9 @@
 #include<unistd.h>
+/*header file goes here*/
 /*
-*man - quote.c
-*Return 0
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+*Return: 1
 */
 int main(void)
 {
