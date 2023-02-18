a37d226ad4c8f3e2e7725e9f3b9746976c46141c
black99hard <yasser99hard@gmail.com>
2023-02-18 10:17:00 +0100

    d

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * *main - find the last digit of the given random numbers
+ * @ch to insert the characters fuond during the  looping
+ * Return: return success if went well
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+    if (ch != 'q' && ch != 'e')
+    {
+        putchar(ch);
+    }
+
+}
+
+putchar(10);
+
+return (0);
+}
