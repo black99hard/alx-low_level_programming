e7947eb6c2f9bbc2cbca7c1f462fa029427b82ff
black99hard <yasser99hard@gmail.com>
2023-02-18 11:17:49 +0100

    d

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * *main - find the last digit of the given random numbers
+ * @ch to insert the characters fuond during the  looping
+ * Return: return success if went well
+ */
+int main(void)
+{
+int ch;
+
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+if (ch == '9')
+{
+for (int hex = 'a'; hex <= 'f'; hex++)
+{
+putchar(hex);
+}
+}
+
+}
+
+putchar(10);
+
+return (0);
+}
