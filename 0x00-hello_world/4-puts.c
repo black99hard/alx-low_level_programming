eb8d6cb83fb1b80a5bcc469ba7b1ae37b250de37
black99hard <yasser99hard@gmail.com>
2023-02-18 16:59:21 +0100

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+/*more header go here*/
 
+
+
+/**
+* main - to print a data type size
+* Return: 0
+*/
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle \n");
-    return (0);
+puts("Programming is like building a multilingual puzzle \n");
+
+return (0);
 }
