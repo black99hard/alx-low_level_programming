c470a519d850d5a7704a739b5186afcb2b4de3b1
black99hard <yasser99hard@gmail.com>
2023-02-21 23:49:45 +0100

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*more header will be here*/
+
+/*
+*main - for the entire code
+*Return: it will return 0
+*/
+int main(void)
+{
+	printf("_putchar \n");
+
+	return (0);
+
+}
---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*more header will be here*/
+
+/*
+*main - for the entire code
+*Return: it will return 0
+*/
+int main(void)
+{
+	int i;
+	i = 89;
+	positive_or_negative(i);
+	
+	return (0);
+
+}
