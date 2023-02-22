9842b98671b00ee73a48a5900c5deef683b59788
black99hard <yasser99hard@gmail.com>
2023-02-22 14:54:12 +0100

    d

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
-/*more header will be here*/
 
-/*
-*main - for the entire code
-*Return: it will return 0
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
 	int i;
-	i = 89;
-	positive_or_negative(i);
-	
-	return (0);
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
