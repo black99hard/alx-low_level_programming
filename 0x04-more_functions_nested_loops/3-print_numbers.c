4356aa68e4eb7d9b3c533f195773d4cd6d473341
black99hard <yasser99hard@gmail.com>
2023-02-25 22:48:55 +0100

    d

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - multiplies two integers
+* Return: addition of a and b
+*/
+void print_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+return (0);
+}
