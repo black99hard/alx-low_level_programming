3d97c3fdd39cf968b00a512215fb83885bf49450
black99hard <yasser99hard@gmail.com>
2023-02-26 00:11:47 +0100

    d

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_diagonal- print 0123456789
+*@n: to determine the line
+*Return: void
+*/
+void print_diagonal(int n)
+{
+int i;
+if (!(n <= 0))
+{
+for (i = 0; i < n; i++)
+{
+_putchar(10);
+}
+}
+else
+{
+_putchar(10);
+}
+}
