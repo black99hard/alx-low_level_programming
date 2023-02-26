142996f1f7a80eb61fdface735cd90653a52f3e2
black99hard <yasser99hard@gmail.com>
2023-02-26 10:16:15 +0100

    d

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,17 +9,18 @@
 */
 void print_square(int size)
 {
-int i, j;
-if (!(size <= 0))
-{
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < (size); j++)
-{
-_putchar('#');
-}
-_putchar(10);
-}
-}
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 
 }
