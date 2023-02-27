8c4aa9a29bbd1f12bbbcc1dda628de49224f3df8
black99hard <yasser99hard@gmail.com>
2023-02-27 22:00:16 +0100

    d

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/*more header*/
+
+/**
+*_puts - to return the string len
+*@str: string
+*Return: void
+**/
+void _puts(char *str)
+{
+	printf(" %s ", str);
+}
