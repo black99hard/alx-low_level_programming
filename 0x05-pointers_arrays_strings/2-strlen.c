056dfde623e39a180c47aa0c2ac7bb9ceb243276
black99hard <yasser99hard@gmail.com>
2023-02-27 21:49:23 +0100

    d

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/*more header*/
+
+/**
+*_strlen - to return the string len
+*@s: string
+*Return: void
+**/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
