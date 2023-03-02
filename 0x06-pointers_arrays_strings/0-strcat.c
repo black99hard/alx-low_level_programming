e862dcd147783ff9ff4074840106761a220ca659
black99hard <yasser99hard@gmail.com>
2023-03-02 14:30:19 +0100

    d

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,17 @@
 **/
 char *_strcat(char *dest, char *src)
 {
-char *s;
-s = strcat(dest, src);
+char *s = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
 return (s);
 }
