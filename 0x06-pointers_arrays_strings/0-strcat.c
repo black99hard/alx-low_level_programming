776298286504901523d0d370424a0eb0b1e75a6f
black99hard <yasser99hard@gmail.com>
2023-03-02 10:17:29 +0100

    d

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* *_strcat- copy th des to src
+*@dest: char
+*@src: char
+* Return: return new modern of string
+**/
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
