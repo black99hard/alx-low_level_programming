12e10fcf6f67c591607d4d0c8eb6c5e9b750c573
black99hard <yasser99hard@gmail.com>
2023-03-02 14:39:48 +0100

    d

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* *_strncat - copy th des to src
+*@dest: char
+*@src: char
+*@n: int
+* Return: return new modern of string
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+return (_strncat(dest, src, n));
+}
