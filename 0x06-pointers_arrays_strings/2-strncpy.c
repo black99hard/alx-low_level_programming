8437f3d52d28a9b856c1b87dc8ba74b326fc3bbf
black99hard <yasser99hard@gmail.com>
2023-03-02 17:06:55 +0100

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,16 @@
 **/
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *s = dest;
+int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n && *(src + i); i++)
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
 
-return (s);
 }
