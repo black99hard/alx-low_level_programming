12205298a0eb89bfa52024b6c9e35e651bd09811
black99hard <yasser99hard@gmail.com>
2023-03-08 00:45:39 +0100

    d

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,13 +9,12 @@
 **/
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-	char *p = s;
+	unsigned int  i = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 
 	}
-return (p);
+return (s);
 }
