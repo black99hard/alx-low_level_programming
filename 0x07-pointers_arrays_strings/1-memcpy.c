bcf3903a06c38661d10602bcadea58e44fb90bba
black99hard <yasser99hard@gmail.com>
2023-03-08 00:56:28 +0100

    d

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memcpy - write a function that check for a digit (0 through 9)
+* @dest: char to check
+* @src: char to check
+* @n: char to check
+* Return:  0 or 1
+**/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	 unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+return (dest);
+}
