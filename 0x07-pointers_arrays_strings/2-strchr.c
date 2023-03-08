7467e658b70324d5546bb30eecf8afbe8c896d3e
black99hard <yasser99hard@gmail.com>
2023-03-08 01:43:37 +0100

    d

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strchr - write a function that
+* @s: char to check
+* @c: char to check
+* Return:  0 or 1
+**/
+char *_strchr(char *s, char c);
+{
+	while (*p)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+
+	if (c == '\0')
+		return (s);
+
+return (NULL);
+}
