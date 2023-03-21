5c1797b023d666fbcb16571f148b0e13cd008430
black99hard <yasser99hard@gmail.com>
2023-03-21 13:59:56 +0100

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,30 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG
+#define DOG
 
-    struct dog
-    {
-        /* data */
-        char *name;
-        float age;
-        char *owner;
-    };
-    
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
